64ce8f377160b06df61dc03c99c18fe069d06fed
Akkeri <akkeriamani@gmail.com>
2022-06-24 05:19:40 +0200

    some-changes

--- src/app/models/User.ts
+++ src/app/models/User.ts
@@ -4,9 +4,9 @@ import { Company } from "./Company";
 
 export class User {
     id: number;
-    name: string = "";
-    username: string = "";
-    email: string = "";
+    name: string;
+    username: string;
+    email: string;
     address: Address;
     phone: string;
     website: string;
--- src/app/users/user-detail/user-detail.component.ts
+++ src/app/users/user-detail/user-detail.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
 import { User } from 'src/app/models/User';
-import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-user-detail',
@@ -9,21 +7,18 @@ import { UserService } from 'src/app/services/user.service';
   styleUrls: ['./user-detail.component.scss']
 })
 export class UserDetailComponent implements OnInit {
-  userId: string;
   user: User;
 
-
-  constructor(private router: Router, private activatedRoute: ActivatedRoute, private userService: UserService) {
-   // store user detail in local storage 
-    if (history.state) {
-      this.user = history.state[0];
-      localStorage.setItem('USER_DETAIL', JSON.stringify(history.state[0]));
-      this.user = JSON.parse(localStorage.getItem("USER_DETAIL") || '{}');
-    }
+  constructor() {
   }
 
   ngOnInit(): void {
-
+  this.handleDetails();
+  this.user = JSON.parse(localStorage.getItem('USER_DETAIL') || "null");
+  
   }
-
+ // store user detail in local storage 
+  handleDetails() {
+    localStorage.setItem('USER_DETAIL', JSON.stringify(history.state[0]));
+  }
 }
